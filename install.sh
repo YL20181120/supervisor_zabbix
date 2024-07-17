4a97984e8400eb04d6e0f2fe824681463b97a6bc
丶大毛 <youjingqiang@vip.qq.com>
2024-07-17 15:30:27 +0800

    1

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 cd /etc/zabbix/zabbix_agent2.d/plugins.d
-wget https://raw.githubusercontent.com/YL20181120//supervisor_zabbix/main/supervisor.conf
+wget https://raw.githubusercontent.com/YL20181120/supervisor_zabbix/main/supervisor.conf
 cd /etc/zabbix/
 mkdir scripts
 cd scripts/
-wget https://raw.githubusercontent.com/YL20181120//supervisor_zabbix/main/script/supervisor.py
+wget https://raw.githubusercontent.com/YL20181120/supervisor_zabbix/main/script/supervisor.py
 chmod a+x supervisor.py 
+systemctl restart zabbix-agent2
+
+
+cd /etc/zabbix/zabbix_agent2.d/plugins.d
+wget https://raw.githubusercontent.com/YL20181120/supervisor_zabbix/main/ssl_expire_check.conf
+cd /etc/zabbix/scripts
+wget https://raw.githubusercontent.com/YL20181120/supervisor_zabbix/main/script/ssl_expire_check.py
+chmod a+x ssl_expire_check.py
 systemctl restart zabbix-agent2
